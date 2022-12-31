a41b5aff77dc170621c76cae4d9859f1edf4e84e
Christian <chriven321@gmail.com>
2022-12-31 18:01:20 +0100

    prova

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 /**
  * https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
  */
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 
 /**
