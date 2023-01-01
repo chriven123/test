7104ae6d1737aa407eaf5607bb9b1bdbdce035db
Christian <chriven321@gmail.com>
2023-01-01 09:59:50 +0100

    prova

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const app = require('./app/app.js');
-const mongoose = require('mongoose');
-//const dotenv = require('dotenv').config();
+import express from "express";
 
-/**
- * https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
- */
-const port = process.env.PORT || 80;
+const app = express();
+const port = 9000;
+app.use("/", (req, res) => {
+  res.json({ message: "Hello From Express App" });
+});
 
-
-/**
- * Configure mongoose
- */
-// mongoose.Promise = global.Promise;
-app.locals.db = mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-
-        console.log("Connected to Database");
-
-        app.listen(port, () => {
-            console.log(`Server listening on port ${port}`);
-        });
-
-    });
+app.listen(9000, () => {
+  console.log(`Starting Server on Port ${port}`);
+});
